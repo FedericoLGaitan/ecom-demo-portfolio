f40ba05f12499dccd4bf4f74f679c0cd1bc83683
ferolez <lopezfederico0221@gmail.com>
2024-10-24 19:07:33 -0300

    adding a button at the home

--- ecom-demo/src/app/page.tsx
+++ ecom-demo/src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
    <main>
-        Hello, this is the home
+        <Button>Click me</Button>
    </main>
   );
 }
