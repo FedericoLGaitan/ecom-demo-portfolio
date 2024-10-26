13b0443510c91c5190734b38833d40dbb628aa4c
ferolez <lopezfederico0221@gmail.com>
2024-10-26 18:28:27 -0300

    footer terminado

--- ecom-demo/src/components/footer.tsx
+++ ecom-demo/src/components/footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { Separator } from "./ui/separator"
 
 const DataFooter = [
     {
@@ -38,12 +39,17 @@ const Footer = () => {
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-md text-gray-500 sm:mb-0 dark:text-gray-400">
                     {DataFooter.map((data) => 
                      <li key={data.id}>
-                           <Link href={data.link}>{data.name}</Link>
+                           <Link href={data.link} className="hover:underline me-4 md:me-6">{data.name}</Link>
                      </li>)}
                     </ul>
                 </div>
+                <Separator className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
+                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                    &copy; 2024
+                    <Link href="#">Federico Lopez Gaitan</Link>
+                </span>
             </div> 
-
+              
         </footer>
     )
 }
